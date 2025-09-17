8f4836d09f29c99cd8f023117298c1e726664387
danxole <danxole@naver.com>
2025-09-17 18:47:43 +0900

    news.py

--- news.py
+++ news.py
@@ -5,7 +5,6 @@ import urllib.request
 import streamlit as st
 import pandas as pd
 
-
 load_dotenv()
 
 NaverId = os.getenv("NaverId") or st.secrets["NaverId"]
@@ -19,25 +18,33 @@ EndDate = st.date_input("끝 날짜 입력")
 
 SearchHistory = text + " : " + StartDate.strftime("%y년 %m월 %d일") + "~" + EndDate.strftime("%y년 %m월 %d일")
 
-
+# session_state 초기화
 if "history" not in st.session_state:
     st.session_state["history"] = []
 
 
 if st. button("검색하기") and text:
+
+    # 입력한 검색어 바꿔서 url에 붙이기
     encText = urllib.parse.quote(text)
     url = f"https://openapi.naver.com/v1/search/news.json?query={encText}&display=100"
 
+    # 검색하기
     request = urllib.request.Request(url)
     request.add_header("X-Naver-Client-Id", NaverId)
     request.add_header("X-Naver-Client-Secret", NaverSecret)
 
+
+    # 검색 기록 저장
     st.session_state["history"].append(SearchHistory)
 
     try:
         with urllib.request.urlopen(request) as response:
+            # 정상적으로 검색됐는지 검사
             rescode = response.getcode()
             if rescode == 200:
+
+                # 복호화하기, 타입 변환해주기
                 ResponseBody = response.read()
                 ResponseResult = ResponseBody.decode("utf-8")
                 ResponseResult = json.loads(ResponseResult)
@@ -47,14 +54,22 @@ if st. button("검색하기") and text:
                 if items:
                     df = pd.DataFrame(items)
 
+                    # 날짜 형식을 바꿔주기
                     df["pubDate"] = pd.to_datetime(df["pubDate"], format = "%a, %d %b %Y %H:%M:%S %z")
                     
+                    # 날짜 필터링
                     FilteredDf = df[
                         (df["pubDate"].dt.date >= StartDate) & (df["pubDate"].dt.date <= EndDate)
                     ]
 
-                    st.write(f"{StartDate}부터 {EndDate}까지 {text}키워드로 {len(FilteredDf)}개의 뉴스가 작성되었습니다.")
-                    st.dataframe(FilteredDf)
+                    if len(FilteredDf) == 0:
+                        st.info("검색 결과가 없습니다.")
+
+                    else:
+                        # 검색 결과가 있으면 출력해주기
+                        st.write(f"{StartDate}부터 {EndDate}까지 {text}키워드로 {len(FilteredDf)}개의 뉴스가 작성되었습니다.")
+                        st.dataframe(FilteredDf)
+                    
                 else:
                     st.info("검색 결과가 없습니다.")
             else:
@@ -64,10 +79,11 @@ if st. button("검색하기") and text:
 
 st.subheader("검색 기록")
 
+# 검색 기록 업데이트해주기
 for history in st.session_state["history"]:
     st.write(history)
     
+# 검색 기록 지우기
 if st.button("기록 삭제"):
     st.session_state.clear()
-
 
